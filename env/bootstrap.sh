2b973924243ff6a8dc623dff1a286ce662f7791a
learnalready <jonlowe3d@gmail.com>
2016-12-05 21:55:25 -0800

    First try

---
+++ env/bootstrap.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+print_step () {
+    echo $1
+}
+
+apt-get update
+apt-get install -y apache2
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
+#apt-get --assume-yes install docker.io
+
+#Install Ansible
+echo "Install Ansible..."
+apt-get install --assume-yes software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+#apt-get -y upgrade
+apt-get install --assume-yes ansible
+sudo -H -u ubuntu echo "LS_COLORS=\$LS_COLORS:'di=0;35:' ; export LS_COLORS" >> /home/ubuntu/.bashrc
+
+echo "Install Docker..."
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+apt-get update
+sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get -y install docker-engine
+sudo service docker start
+sleep 3
+echo "Test Docker..."
+sudo docker run hello-world
+
+exit 0
+
+
+#setup Jenkins
+echo "Install Jenkins..."
+mkdir -p ~/jenkins
+sudo chmod ugo+rw ~/jenkins   # maybe permissiosn too much not sure
+sudo docker run -p 8080:8080 -p 50000:50000 -v ~/jenkins:/var/jenkins_home jenkins
+
+# Goal put Ruby in docker file and add cucumber to that container for now just pull ruby into Host
+echo "Install Ruby..."
+sudo apt-get -y install ruby-full
+sudo gem install cucumber
+sudo gem install gherkin
+sudo apt-get -y install default-jre
+
+
+# install chrome/chrome driver
+echo "Install Chrome..."
+sudo apt-get -y install libxss1 libappindicator1 libindicator7
+echo "Downloading Chrome..."
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome*.deb
+sudo apt-get install -f
+sudo apt-get install xvfb # headless chrome
+sudo apt-get install unzip
+
+echo "Install ChromeDriver..."
+wget -N http://chromedriver.storage.googleapis.com/2.20/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+
+sudo mv -f chromedriver /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+
+
+# enable insecure registry DOCKER_OPTS="--insecure-registry 10.100.198.200:5000"in file /etc/default/docker/ 
+#ansible-playbook -i "localhost," -c local docker.yml
+
+
