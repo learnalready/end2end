e3a8e05b6a66b912f2a54f66b38e2c42e93ef72f
learnalready <jonlowe3d@gmail.com>
2016-12-06 14:21:38 -0800

    Reboot to fix IO issue

--- env/vup_dotnet.bat
+++ env/vup_dotnet.bat
@@ -2,3 +2,5 @@ SET vm=.net
 REM first time need this plugin
 vagrant plugin install vagrant-host-shell
 vagrant up
+REM Not sure how to get around reboot yet, some sym link issue causes I/O errors reboot seems to fix.
+vagrant reload 
